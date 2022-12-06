7703279becef3e74b65b91360a5a66dd07ef1304
triciachow0212 <suyeechow0212@gmail.com>
2022-12-06 11:14:37 +0800

    Fix mobile alignment

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,26 +5,30 @@ export default function Main() {
   return (
     <main className="lg:px-32 md:px-20 px-4 pt-20 mb-10">
       <div className="flex flex-col lg:flex-row justify-center">
-        <div className="flex flex-col justify-center items-center gap-y-8 lg:w-[614px] w-full">
-          <h1 className="font-title text-5xl text-amber-700">
-            An Ambient <br /> Dining Experience
-          </h1>
-          <p>
-            Join us at the table as you dine for the perfect meal. <br /> We
-            treat all of our customers with utmost care and service.
-          </p>
-          <p>Have a toast and enjoy our fine drinks while you’re at it.</p>
-          <div className="flex gap-x-8">
-            <Link to="contact">
-              <button className="bg-neutral-900 text-white px-4 py-2 flex justify-between w-[180px] font-title">
+        <div className="flex items-center justify-center lg:w-[614px] w-full">
+          <div className="flex flex-col gap-y-8">
+            <h1 className="font-title text-5xl text-amber-700">
+              An Ambient <br /> Dining Experience
+            </h1>
+            <p>
+              Join us at the table as you dine for the perfect meal. <br /> We
+              treat all of our customers with utmost care and service.
+            </p>
+            <p>Have a toast and enjoy our fine drinks while you’re at it.</p>
+            <div className="flex gap-x-8">
+              <Link
+                to="contact"
+                className="bg-neutral-900 text-white px-4 py-2 flex justify-between w-36 font-title"
+              >
                 Contact Us <ArrowRight />
-              </button>
-            </Link>
-            <Link to="menu">
-              <button className="bg-amber-700 text-white px-4 py-2 flex justify-between w-[180px] font-title">
+              </Link>
+              <Link
+                to="menu"
+                className="bg-amber-700 text-white px-4 py-2 flex justify-between w-36 font-title"
+              >
                 Menu <ArrowRight />
-              </button>
-            </Link>
+              </Link>
+            </div>
           </div>
         </div>
         <img
